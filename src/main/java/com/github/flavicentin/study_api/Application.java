e1201e6ba6b07dd9c0da4cee476ca2b92aa77826
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-02 22:37:58 -0300

    Fim da aula de API usando Spring 03-02-2023

---
+++ src/main/java/com/github/flavicentin/study_api/Application.java
@@ -0,0 +1,27 @@
+package com.github.flavicentin.study_api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner{
+
+	private static Logger LOG = LoggerFactory.getLogger(Application.class);
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Implementando run");
+		LOG.info("Testando o log");
+		
+	}
+
+}
---
+++ src/main/java/com/github/flavicentin/study_api/service/PessoaService.java
@@ -0,0 +1,35 @@
+package com.github.flavicentin.study_api.service;
+import java.util.ArrayList;
+import java.util.List;
+import com.github.flavicentin.study_api.model.Pessoa;
+
+public class PessoaService {
+	private List<Pessoa> pessoas;
+	
+	public PessoaService() {
+		load();
+	}
+	
+	private void load() {
+		pessoas = new ArrayList<>();
+		Pessoa pessoa = new Pessoa ();
+		pessoa.setId(1L);
+		pessoa.setNome("Flávio");
+		pessoas.add(pessoa);
+		
+		pessoa = new Pessoa ();
+		pessoa.setId(2L);
+		pessoa.setNome("João");
+		pessoas.add(pessoa);
+		
+		pessoa = new Pessoa ();
+		pessoa.setId(3L);
+		pessoa.setNome("Julio");
+		pessoas.add(pessoa);
+	}
+	
+	public List<Pessoa>list(){
+		return pessoas;
+	}
+	
+}
